7fdf77df9c3897d4c25cd43a81e875210a7dd1fa
Liam Kandel <liam.kandel@gmail.com>
2025-01-31 16:50:00 -0500

    Initial commit

---
+++ Website/auth.py
@@ -0,0 +1,16 @@
+from flask import Blueprint
+from flask import render_template
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return render_template("login.html", text="Testing")
+
+@auth.route("/logout")
+def logout():
+    return render_template("home.html")
+
+@auth.route("/signup")
+def signup():
+    return render_template("signup.html")
